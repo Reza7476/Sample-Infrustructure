e764cbf67ac3708e3933d8db02a102b50c77a445
Reza Dehghani <reza.dehghani@medis.land>
2025-03-10 14:55:29 +0330

    create entity without base entity

--- src/Infrastructure/Sample.Persistence.EF/Migrations/20250310111850_CreateProject.cs
+++ src/Infrastructure/Sample.Persistence.EF/Migrations/20250310111850_CreateProject.cs
@@ -5,11 +5,24 @@
 namespace Sample.Persistence.EF.Migrations
 {
     /// <inheritdoc />
-    public partial class InitialProject : Migration
+    public partial class CreateProject : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+
             migrationBuilder.CreateTable(
                 name: "Users",
                 columns: table => new
@@ -17,12 +30,13 @@ namespace Sample.Persistence.EF.Migrations
                     Id = table.Column<long>(type: "bigint", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
                     MacId = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Mobile = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Mobile = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     ProfileUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     Gender = table.Column<int>(type: "int", nullable: false),
+                    UserStatus = table.Column<byte>(type: "tinyint", nullable: false, defaultValue: (byte)0),
                     NationalCode = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     CreatedAt = table.Column<long>(type: "bigint", nullable: false),
                     CreatedBy = table.Column<long>(type: "bigint", maxLength: 36, nullable: true),
@@ -66,9 +80,38 @@ namespace Sample.Persistence.EF.Migrations
                         principalColumn: "Id");
                 });
 
+            migrationBuilder.CreateTable(
+                name: "UserHangfires",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<long>(type: "bigint", nullable: false),
+                    Status = table.Column<byte>(type: "tinyint", nullable: false),
+                    CreatedAt = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedBy = table.Column<long>(type: "bigint", maxLength: 36, nullable: true),
+                    UpdatedAt = table.Column<long>(type: "bigint", nullable: true),
+                    UpdatedBy = table.Column<long>(type: "bigint", maxLength: 36, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserHangfires", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserHangfires_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
             migrationBuilder.CreateIndex(
                 name: "IX_Medias_UserId",
                 table: "Medias",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserHangfires_UserId",
+                table: "UserHangfires",
                 column: "UserId");
         }
 
@@ -76,7 +119,13 @@ namespace Sample.Persistence.EF.Migrations
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
                 name: "Medias");
+
+            migrationBuilder.DropTable(
+                name: "UserHangfires");
 
             migrationBuilder.DropTable(
                 name: "Users");
